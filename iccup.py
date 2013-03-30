90de2403e9eedb3d07192dafcdd97a3b1d52a025
Malcolm Sharpe <sharpe.malcolm@gmail.com>
2013-03-30 03:51:48 -0700

    Force stalling to obey iCCup's robots.txt.
    Sadly slows down the populating drastically.

--- iccup.py
+++ iccup.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import urllib2
 
 
@@ -6,16 +7,31 @@ class URLError(Exception):
   pass
 
 
+# http://iccup.com/robots.txt
+# User-agent: *
+# Crawl-delay: 15
+CRAWL_DELAY = 15
+last_crawl = None
+
 RETRIES = 30
 TIMEOUT = 20
 def urlopen(url):
+  global last_crawl
   retry = RETRIES
   while retry > 0:
     retry -= 1
     try:
+      now = time.time()
+      if last_crawl is not None:
+        stall = last_crawl + CRAWL_DELAY - now
+        if stall > 0:
+          print 'urlopen stalling for %.1f seconds to obey robots.txt' % stall
+          time.sleep(stall)
+
       print 'Opening %s ...' % url
       sys.stdout.flush()
       lines = list( urllib2.urlopen(url, timeout=TIMEOUT) )
+      last_crawl = time.time()
       return lines
     except Exception:
       pass
