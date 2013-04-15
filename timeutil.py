0547c2b9823ff642f7c0bba4472564fee63c3165
malcolmsharpe <sharpe.malcolm@gmail.com>
2013-04-15 05:40:07 +0000

    Fix timestamp handling to work regardless of local timezone.

--- timeutil.py
+++ timeutil.py
@@ -2,10 +2,12 @@ import datetime
 
 def format_timestamp(timestamp):
   if timestamp is not None:
-    dt = datetime.datetime.fromtimestamp(timestamp)
+    dt = datetime.datetime.utcfromtimestamp(timestamp)
     # Convert from MSD to PDT.
     # Unfortunately some timestamps are in MSK and some in MSD.
-    dt = dt - datetime.timedelta(hours=12)
+    # Also the timestamps are completely broken--their meaning is unknown.
+    # The number here gives the correct result, but I don't know why.
+    dt = dt - datetime.timedelta(hours=17)
     return dt.strftime('%a %b %d %H:%M PDT')
   else:
     return 'unknown'
