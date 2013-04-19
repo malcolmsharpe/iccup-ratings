ee13d31a4e7435124a089c84d46c98b68016e458
malcolmsharpe <sharpe.malcolm@gmail.com>
2013-04-19 07:07:11 +0000

    Disable games dump for now since it's not getting use.

--- publish.sh
+++ publish.sh
@@ -4,7 +4,8 @@ set -x
 python dump_games_json.py
 cp static/leader.css ../www/static/leader.css
 cp html/leaderboard.html ../www/html/leaderboard.html
-cp html/games.json ../www/games.json
+# Disable games dump for now since it's not getting use.
+#cp html/games.json ../www/games.json
 cp html/maps.html ../www/html/maps.html
 cd ../www
 git commit -a -m 'Publish latest data.'
